b8039ce6261af692a5c0f73fd936940e80132cd6
Aleks Rūtiņš <aleks@rutins.com>
2022-05-06 16:45:53 -0400

    Padding

--- utils/type-text.go
+++ utils/type-text.go
@@ -2,14 +2,21 @@ package utils
 
 import (
 	"fmt"
+	"strings"
 	"time"
 )
 
 func TypeText(text string, interval time.Duration) {
-	fmt.Print("\x1b[2J\x1b[H") // Clear screen
-	for i := 0; i < len(text); i++ {
-		fmt.Print(string(text[i]))
-		time.Sleep(interval)
+	fmt.Println("\x1b[2J\x1b[H") // Clear screen
+	lines := strings.Split(text, "\n")
+	for i := 0; i < len(lines); i++ {
+		line := lines[i]
+		fmt.Print("  ")
+		for j := 0; j < len(line); j++ {
+			fmt.Print(string(line[j]))
+			time.Sleep(interval)
+		}
+		fmt.Println()
 	}
 	fmt.Println()
 	time.Sleep(2 * time.Second)
